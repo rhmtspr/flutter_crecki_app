0ec74d9d714b8ab1da58fe7d5bf2f22790a47909
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-24 20:35:24 +0800

    creae injection.dart

---
+++ lib/injection.dart
@@ -0,0 +1,28 @@
+import 'package:get_it/get_it.dart';
+import 'package:flutter_cracky_app/data/datasources/ai_local_datasource.dart';
+import 'package:flutter_cracky_app/data/repositories/crack_repository_impl.dart';
+import 'package:flutter_cracky_app/domain/repositories/crack_repository.dart';
+import 'package:flutter_cracky_app/domain/usecases/analyze_structural_damage.dart';
+import 'package:flutter_cracky_app/presentation/bloc/camera_bloc.dart';
+import 'package:flutter_cracky_app/presentation/bloc/crack_bloc.dart';
+
+final locator = GetIt.instance;
+
+Future<void> initInjection() async {
+  // Bloc
+  locator.registerFactory(() => CameraBloc());
+  locator.registerFactory(() => CrackDetectionBloc(locator()));
+
+  // Use cases
+  locator.registerLazySingleton(() => AnalyzeStructuralDamage(locator()));
+
+  // Repository
+  locator.registerLazySingleton<CrackRepository>(
+    () => CrackRepositoryImpl(locator()),
+  );
+
+  // Data sources
+  final aiLocalDataSource = AiLocalDataSource();
+  await aiLocalDataSource.init();
+  locator.registerLazySingleton(() => aiLocalDataSource);
+}
