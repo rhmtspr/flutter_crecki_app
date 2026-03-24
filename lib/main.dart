bb23d6618756f6197e34ed5a6f647e67bf134d49
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-24 21:56:40 +0800

    add child of multi provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_cracky_app/core/utils/router_observer.dart';
 import 'package:flutter_cracky_app/injection.dart';
 import 'package:flutter_cracky_app/presentation/bloc/camera_bloc.dart';
 import 'package:flutter_cracky_app/presentation/bloc/crack_bloc.dart';
+import 'package:flutter_cracky_app/presentation/pages/camera_page.dart';
+import 'package:flutter_cracky_app/presentation/pages/home_page.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   initInjection();
-
   runApp(MainApp());
 }
 
@@ -21,6 +24,25 @@ class MainApp extends StatelessWidget {
         BlocProvider(create: (_) => locator<CameraBloc>()),
         BlocProvider(create: (_) => locator<CrackDetectionBloc>()),
       ],
+      child: MaterialApp(
+        title: "Flutter Crecki App",
+        home: HomePage(),
+        navigatorObservers: [routeObserver],
+        onGenerateRoute: (RouteSettings settings) {
+          switch (settings.name) {
+            case "/home":
+              return MaterialPageRoute(builder: (_) => HomePage());
+            default:
+              return MaterialPageRoute(
+                builder: (_) {
+                  return Scaffold(
+                    body: Center(child: Text("Page not found :(")),
+                  );
+                },
+              );
+          }
+        },
+      ),
     );
   }
 }
