4715ac0fa6f51ec70dfa78bb652fa5461cfff242
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-26 01:40:56 +0800

    create app_theme.dart

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_cracky_app/core/theme/app_colors.dart';
+import 'package:flutter_cracky_app/core/theme/app_text_style.dart';
+
+class AppTheme {
+  static ThemeData get lightTheme {
+    return ThemeData(
+      colorSchemeSeed: AppColors.blue.color,
+      brightness: Brightness.light,
+      textTheme: _textTheme,
+      useMaterial3: true,
+      appBarTheme: _appBarTheme,
+    );
+  }
+
+  static ThemeData get darkTheme {
+    return ThemeData(
+      colorSchemeSeed: AppColors.blue.color,
+      brightness: Brightness.dark,
+      textTheme: _textTheme,
+      useMaterial3: true,
+      appBarTheme: _appBarTheme,
+    );
+  }
+
+  static TextTheme get _textTheme {
+    return TextTheme(
+      displayLarge: AppTextStyles.displayLarge,
+      displayMedium: AppTextStyles.displayMedium,
+      displaySmall: AppTextStyles.displaySmall,
+      headlineLarge: AppTextStyles.headlineLarge,
+      headlineMedium: AppTextStyles.headlineMedium,
+      headlineSmall: AppTextStyles.headlineSmall,
+      titleLarge: AppTextStyles.titleLarge,
+      titleMedium: AppTextStyles.titleMedium,
+      titleSmall: AppTextStyles.titleSmall,
+      bodyLarge: AppTextStyles.bodyLargeBold,
+      bodyMedium: AppTextStyles.bodyLargeMedium,
+      bodySmall: AppTextStyles.bodyLargeRegular,
+      labelLarge: AppTextStyles.labelLarge,
+      labelMedium: AppTextStyles.labelMedium,
+      labelSmall: AppTextStyles.labelSmall,
+    );
+  }
+
+  static AppBarTheme get _appBarTheme {
+    return AppBarTheme(
+      toolbarTextStyle: _textTheme.titleLarge,
+      shape: const BeveledRectangleBorder(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(14),
+          bottomRight: Radius.circular(14),
+        ),
+      ),
+    );
+  }
+}
