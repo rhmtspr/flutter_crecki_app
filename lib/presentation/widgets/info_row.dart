3fee94797ead420370d379ba5221c778b01b2162
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-26 01:42:12 +0800

    create info_row.dart

---
+++ lib/presentation/widgets/info_row.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class InfoRow extends StatelessWidget {
+  final String title;
+  final String value;
+
+  const InfoRow({super.key, required this.title, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(title, style: Theme.of(context).textTheme.bodySmall),
+        Text(value, style: const TextStyle(fontWeight: FontWeight.bold)),
+      ],
+    );
+  }
+}
