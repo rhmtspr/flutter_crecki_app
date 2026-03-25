256930316b0cfdfcf2602363e12216a78899cf58
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-25 16:47:39 +0800

    create loadModel method and add _imageToByteListFloat32 method

--- lib/data/datasources/ai_local_datasource.dart
+++ lib/data/datasources/ai_local_datasource.dart
@@ -1,17 +1,69 @@
-import 'package:tflite_flutter/tflite_flutter.dart';
+import "dart:io";
+import "dart:typed_data";
+import "package:flutter/services.dart";
+import "package:tflite_flutter/tflite_flutter.dart";
+import "package:image/image.dart" as img;
 
-class AiLocalDataSource {
-  late Interpreter _interpreter;
+class AiLocalDatasource {
+  Interpreter? _interpreter;
+  List<String>? _labels;
 
-  Future<void> init() async {
-    _interpreter = await Interpreter.fromAsset('assets/cracki_model.tflite');
+  Future<void> loadModel() async {
+    try {
+      _interpreter = await Interpreter.fromAsset("assets/cracki_model.tflite");
+      final labelData = await rootBundle.loadString("assets/labels.txt");
+      _labels = labelData.split('\n');
+      print("Model and labels loaded successfully");
+    } catch (e) {
+      print("Error loading model: $e");
+    }
   }
 
-  List<double> runModel(List input) {
-    final output = List.generate(1, (_) => List.filled(3, 0.0));
+  Future<Map<String, dynamic>> runInference(File imageFile) async {
+    if (_interpreter == null) throw Exception("interpreter not initialized");
 
-    _interpreter.run(input, output);
+    final imageData = await imageFile.readAsBytes();
+    img.Image? originalImage = img.decodeImage(imageData);
+    img.Image resizedImage = img.copyResize(
+      originalImage!,
+      width: 224,
+      height: 224,
+    );
 
-    return output[0];
+    var input = _imageToByteListFloat32(resizedImage, 224);
+
+    var output = List.filled(1 * 3, 0.0).reshape([1, 3]);
+
+    _interpreter!.run(input, output);
+    int classIndex = -1;
+    double highScoringProb = 0.0;
+
+    for (int i = 0; i < output[0].length; i++) {
+      if (output[0][i] > highScoringProb) {
+        highScoringProb = output[0][i];
+        classIndex = i;
+      }
+    }
+
+    return {"label": _labels![classIndex], "confidence": highScoringProb};
+  }
+
+  Uint8List _imageToByteListFloat32(img.Image image, int inputSize) {
+    var convertedBytes = Float32List(1 * inputSize * inputSize * 3);
+    var buffer = Float32List.view(convertedBytes.buffer);
+    int pixelIndex = 0;
+    for (var y = 0; y < inputSize; y++) {
+      for (var x = 0; x < inputSize; x++) {
+        var pixel = image.getPixel(x, y);
+        buffer[pixelIndex++] = pixel.r / 255.0;
+        buffer[pixelIndex++] = pixel.g / 255.0;
+        buffer[pixelIndex++] = pixel.b / 255.0;
+      }
+    }
+    return convertedBytes.buffer.asUint8List();
+  }
+
+  void close() {
+    _interpreter?.close();
   }
 }
