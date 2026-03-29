aad2e07dc37003affd9f7ef61e1e3827e366ca2c
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-29 19:30:44 +0800

    add loadYoloModel and analyzeImage

--- lib/data/datasources/ai_local_datasource.dart
+++ lib/data/datasources/ai_local_datasource.dart
@@ -3,20 +3,34 @@ import "dart:typed_data";
 import "package:flutter/services.dart";
 import "package:tflite_flutter/tflite_flutter.dart";
 import "package:image/image.dart" as img;
+import "package:flutter_vision/flutter_vision.dart";
 
 class AiLocalDatasource {
+  late FlutterVision vision;
   Interpreter? _interpreter;
   List<String>? _labels;
 
-  Future<void> loadModel() async {
-    try {
-      _interpreter = await Interpreter.fromAsset("assets/cracki_model.tflite");
-      final labelData = await rootBundle.loadString("assets/labels.txt");
-      _labels = labelData.split('\n');
-      print("Model and labels loaded successfully");
-    } catch (e) {
-      print("Error loading model: $e");
-    }
+  // Future<void> loadModel() async {
+  //   try {
+  //     _interpreter = await Interpreter.fromAsset("assets/cracki_model.tflite");
+  //     final labelData = await rootBundle.loadString("assets/labels.txt");
+  //     _labels = labelData.split('\n');
+  //     print("Model and labels loaded successfully");
+  //   } catch (e) {
+  //     print("Error loading model: $e");
+  //   }
+  // }
+
+  Future<void> loadYoloModel() async {
+    vision = FlutterVision();
+
+    await vision.loadYoloModel(
+      modelPath: "assets/best_float32.tflite",
+      labels: "assets/label.txt",
+      modelVersion: "yolov8",
+      numThreads: 2,
+      useGpu: true,
+    );
   }
 
   Future<Map<String, dynamic>> runInference(File imageFile) async {
@@ -65,5 +79,52 @@ class AiLocalDatasource {
 
   void close() {
     _interpreter?.close();
+  }
+
+  Future<Map<String, dynamic>> analyzeImage(
+    Uint8List bytes,
+    int h,
+    int w,
+  ) async {
+    final results = await vision.yoloOnImage(
+      bytesList: bytes,
+      imageHeight: h,
+      imageWidth: w,
+      iouThreshold: 0.4,
+      confThreshold: 0.3,
+      classThreshold: 0.5,
+    );
+
+    if (results.isEmpty) return {"score": 0, "status": "Safe"};
+
+    int crackCount = results.length;
+    double totalArea = 0;
+    double maxIndivRatio = 0;
+
+    for (var crack in results) {
+      double boxWidth = crack["box"][2] - crack["box"][0];
+      double boxHeight = crack["box"][3] - crack["box"][1];
+      double area = (boxWidth * boxHeight) / (h * 2) * 100;
+
+      totalArea += area;
+      if (area > maxIndivRatio) maxIndivRatio = area;
+    }
+
+    double crowdingScore = (crackCount.clamp(0, 10) / 10) * 100;
+    double intensityScore = totalArea * 5;
+    double worstScore = maxIndivRatio * 3;
+
+    double finalScore =
+        (crowdingScore * 0.2) + (intensityScore * 0.3) + (worstScore * 0.5);
+    finalScore = finalScore.clamp(0, 100);
+
+    String status = "Safe";
+    if (crackCount > 6 || worstScore > 60) {
+      status = "Danger";
+    } else if (finalScore > 35) {
+      status = "Warning";
+    }
+
+    return {"score": finalScore, "status": status, "count": crackCount};
   }
 }
